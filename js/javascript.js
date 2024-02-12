e485240454e3ad4bf4fbbc780b381a5dc0bde179
Ussama0 <ussamaossama09@gmail.com>
2024-02-12 17:54:38 +0200

    final edit

--- js/javascript.js
+++ js/javascript.js
@@ -3,5 +3,5 @@ let bgImg = ["001.jpg", "002.jpg", "003.jpg"];
 
 setInterval(function () {
   let random = Math.floor(Math.random() * bgImg.length);
-  landing.style.backgroundImage = `url(/images/${bgImg[random]})`;
+  landing.style.backgroundImage = `url(images/${bgImg[random]})`;
 }, 5000);
