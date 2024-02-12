3c124fc0e7bf29869dd8fa71ada088c45473bc88
Ussama0 <ussamaossama09@gmail.com>
2024-02-12 17:41:07 +0200

    adding server files

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+const landing = document.getElementById("landing");
+let bgImg = ["001.jpg", "002.jpg", "003.jpg"];
+
+setInterval(function () {
+  let random = Math.floor(Math.random() * bgImg.length);
+  landing.style.backgroundImage = `url(/images/${bgImg[random]})`;
+}, 5000);
